d7c19f6b96a2c9fd3a1a2660ff6e14246511a462
Antonio Tapiador del Dujo <atapiador@dit.upm.es>
2011-04-19 12:00:18 +0200

    devise:install

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '3.0.4'
 
 gem 'sqlite3'
 
+# User authentication
+gem 'devise'
+
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,8 @@ Planet::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # Default mailer host for Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
 
